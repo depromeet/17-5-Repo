84ec6559f471a7780ca7f0d90fd597545d2ca1a8
nick <andantej281@gmail.com>
2025-08-14 10:22:19 +0900

    init: 멀티모듈 프로젝트 세팅

---
+++ user/src/test/java/com/dpm05/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpm05.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
