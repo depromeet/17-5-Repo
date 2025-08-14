497e9741d8d3a68a5214c8aae48e0342c95f873a
nick <andantej281@gmail.com>
2025-08-14 15:27:02 +0900

    feat: 소셜 로그인 API용 DTO 클래스 추가
    - SocialLoginRequest DTO 추가
    - SocialLoginResponse DTO 추가
    - OAuth Provider별 로그인 요청/응답 처리 지원

---
+++ user/src/main/java/com/dpm05/user/dto/SocialLoginRequest.java
@@ -0,0 +1,16 @@
+package com.dpm05.user.dto;
+
+import com.dpm05.user.domain.OAuthProvider;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SocialLoginRequest {
+    private OAuthProvider provider;
+    private String authCode;
+    private String email;      // Apple의 경우 클라이언트에서 제공
+    private String nickname;   // Apple의 경우 클라이언트에서 제공
+}
