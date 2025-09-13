11796c5ff4c2f5914f75ddc67c2bd27b3daaa095
nick <andantej281@gmail.com>
2025-09-14 00:35:24 +0900

    feat. dto, entity, command 간의 변환해주는 매퍼 클래스 개발

---
+++ stock-diary/src/main/java/com/ogd/stockdiary/application/retrospection/dto/mapper/RetrospectionMapper.java
@@ -0,0 +1,60 @@
+package com.ogd.stockdiary.application.retrospection.dto.mapper;
+
+import com.ogd.stockdiary.application.retrospection.dto.request.CreateRetrospectionRequest;
+import com.ogd.stockdiary.application.retrospection.dto.response.CreateRetrospectionResponse;
+import com.ogd.stockdiary.domain.retrospection.entity.Order;
+import com.ogd.stockdiary.domain.retrospection.entity.Retrospection;
+import com.ogd.stockdiary.domain.retrospection.port.in.CreateRetrospectionCommand;
+import com.ogd.stockdiary.domain.user.entity.User;
+
+public class RetrospectionMapper {
+
+    public static CreateRetrospectionCommand toCommand(CreateRetrospectionRequest request, Long userId) {
+        return new CreateRetrospectionCommand(
+            userId,
+            request.getSymbol(),
+            request.getMarket(),
+            request.getOrderType(),
+            request.getPrice(),
+            request.getCurrency(),
+            request.getVolume(),
+            request.getOrderDate(),
+            request.getReturnRate()
+        );
+    }
+
+    public static CreateRetrospectionResponse toResponse(Retrospection retrospection) {
+        return new CreateRetrospectionResponse(
+            retrospection.getId(),
+            retrospection.getUser().getId(),
+            retrospection.getSymbol(),
+            retrospection.getMarket(),
+            retrospection.getOrder().getOrderType(),
+            retrospection.getOrder().getPrice(),
+            retrospection.getOrder().getCurrency(),
+            retrospection.getOrder().getVolume(),
+            retrospection.getOrder().getOrderDate(),
+            retrospection.getReturnRate(),
+            retrospection.getCreatedAt(),
+            retrospection.getUpdatedAt()
+        );
+    }
+
+    public static Retrospection toEntity(CreateRetrospectionCommand command, User user) {
+        Order order = new Order(
+            command.orderType(),
+            command.price(),
+            command.currency(),
+            command.volume(),
+            command.orderDate()
+        );
+
+        return new Retrospection(
+            user,
+            command.symbol(),
+            command.market(),
+            order,
+            command.returnRate()
+        );
+    }
+}
