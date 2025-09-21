cb5e00070bcec9375642fb915c0ea93506d24538
nick <andantej281@gmail.com>
2025-09-22 08:18:51 +0900

    feat: 투자 감정 구현

---
+++ stock-diary/src/main/java/com/ogd/stockdiary/domain/retrospection/entity/InvestmentEmotion.java
@@ -0,0 +1,14 @@
+package com.ogd.stockdiary.domain.retrospection.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum InvestmentEmotion {
+
+  ANXIETY(1, "불안"), IMPULSE(2, "충동"), MINDLESSNESS(3, "무념무상"), CONFIDENCE(4, "자신감"), CONVICTION(5, "확신");
+
+  private final int value;
+  private final String description;
+}
