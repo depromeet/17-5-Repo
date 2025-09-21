902b375d1bc8fe1ed30431986f8636af4784375d
nick <andantej281@gmail.com>
2025-09-22 00:19:48 +0900

    feat: 투자원칙 DTO 및 매퍼 추가

---
+++ stock-diary/src/main/java/com/ogd/stockdiary/application/investmentprinciple/dto/request/UpdatePrincipleRequest.java
@@ -0,0 +1,18 @@
+package com.ogd.stockdiary.application.investmentprinciple.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Getter
+@NoArgsConstructor
+@Schema(description = "투자원칙 수정 요청 정보")
+public class UpdatePrincipleRequest {
+
+  @Schema(description = "수정할 투자원칙 내용", example = "손절매는 반드시 5% 이내에서", requiredMode = Schema.RequiredMode.REQUIRED)
+  @NotBlank(message = "투자원칙 내용은 필수입니다.")
+  private String principle;
+}
