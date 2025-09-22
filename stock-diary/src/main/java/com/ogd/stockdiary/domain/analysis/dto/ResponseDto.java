33213e112352c26e64571e6d0634879452be0e16
yeonsoo <dfh56541@gmail.com>
2025-09-22 17:58:54 +0900

    refactor: 불필요한 파일 삭제
    Signed-off-by: yeonsoo <dfh56541@gmail.com>

--- stock-diary/src/main/java/com/ogd/stockdiary/domain/analysis/dto/ResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.ogd.stockdiary.domain.analysis.dto;
-
-public record ResponseDto(Choices choices) {
-  public record Choices(Delta delta) {}
-
-  public record Delta(String content) {}
-}
