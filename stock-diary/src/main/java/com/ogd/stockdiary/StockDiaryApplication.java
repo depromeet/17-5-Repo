0d9a38e3093855edb75235cc7e0242e7c12e61bf
nick <andantej281@gmail.com>
2025-09-24 20:17:18 +0900

    fix: feign client bean 중복 에러 해결 - @EnableFeignClients 어노테이션이 StockDiaryApplication 과 FeignConfiguration 에 모두 정의되어서 발생했던 문제 해결

--- stock-diary/src/main/java/com/ogd/stockdiary/StockDiaryApplication.java
+++ stock-diary/src/main/java/com/ogd/stockdiary/StockDiaryApplication.java
@@ -3,10 +3,8 @@ package com.ogd.stockdiary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@EnableFeignClients
 public class StockDiaryApplication {
 
     public static void main(String[] args) {
