54579fbe56f6d41201af8edd9576210e698c87d7
nick <andantej281@gmail.com>
2025-10-27 22:47:47 +0900

    feat: 투자원칙 Command DTO에 principleType 추가

--- stock-diary/src/main/java/com/ogd/stockdiary/domain/investmentprinciple/dto/CreateMultiplePrinciplesCommand.java
+++ stock-diary/src/main/java/com/ogd/stockdiary/domain/investmentprinciple/dto/CreateMultiplePrinciplesCommand.java
@@ -2,6 +2,8 @@ package com.ogd.stockdiary.domain.investmentprinciple.dto;
 
 import java.util.List;
 
+import com.ogd.stockdiary.domain.investmentprinciple.entity.PrincipleType;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +12,6 @@ import lombok.RequiredArgsConstructor;
 public class CreateMultiplePrinciplesCommand {
 
     private final Long userId;
+    private final PrincipleType principleType;
     private final List<String> principles;
 }
