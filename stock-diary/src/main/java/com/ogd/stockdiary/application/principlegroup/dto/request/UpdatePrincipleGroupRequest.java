13436e44ff9f7fd858742982e160ee8d8a20d627
nick <andantej281@gmail.com>
2025-10-29 08:21:02 +0900

    feat: Add principleType to group update API
    - UpdatePrincipleGroupRequest에 principleType 필드 추가 (선택값)
    - UpdatePrincipleGroupCommand에 principleType 필드 추가
    - PrincipleGroup Entity에 updatePrincipleType 메서드 추가
    - PrincipleGroupService에서 principleType 업데이트 로직 추가

--- stock-diary/src/main/java/com/ogd/stockdiary/application/principlegroup/dto/request/UpdatePrincipleGroupRequest.java
+++ stock-diary/src/main/java/com/ogd/stockdiary/application/principlegroup/dto/request/UpdatePrincipleGroupRequest.java
@@ -2,6 +2,8 @@ package com.ogd.stockdiary.application.principlegroup.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import com.ogd.stockdiary.domain.investmentprinciple.entity.PrincipleType;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,4 +18,7 @@ public class UpdatePrincipleGroupRequest {
     @Schema(description = "그룹명 (이모지 포함 가능)", example = "💪 초보자를 위한 매도 원칙", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "그룹명은 필수입니다")
     private String groupName;
+
+    @Schema(description = "투자원칙 타입 (BUY: 매수, SELL: 매도) - 선택사항", example = "SELL")
+    private PrincipleType principleType;
 }
--- stock-diary/src/main/java/com/ogd/stockdiary/domain/principlegroup/dto/UpdatePrincipleGroupCommand.java
+++ stock-diary/src/main/java/com/ogd/stockdiary/domain/principlegroup/dto/UpdatePrincipleGroupCommand.java
@@ -1,5 +1,7 @@
 package com.ogd.stockdiary.domain.principlegroup.dto;
 
+import com.ogd.stockdiary.domain.investmentprinciple.entity.PrincipleType;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,4 +12,5 @@ public class UpdatePrincipleGroupCommand {
     private Long groupId;
     private Long userId;
     private String groupName;
+    private PrincipleType principleType;
 }
