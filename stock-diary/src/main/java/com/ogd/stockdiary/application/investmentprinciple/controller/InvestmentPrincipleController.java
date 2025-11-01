996d1b985f3e3b034db39da79bd8b1eb904ea01f
조익현/엔마스터서비스팀/ND <ikhyeon.cho@nondooray.com>
2025-11-01 15:17:04 +0900

    refactor: 수정 api http method 수정 - PUT -> PATCH로

--- stock-diary/src/main/java/com/ogd/stockdiary/application/investmentprinciple/controller/InvestmentPrincipleController.java
+++ stock-diary/src/main/java/com/ogd/stockdiary/application/investmentprinciple/controller/InvestmentPrincipleController.java
@@ -4,16 +4,7 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ogd.stockdiary.application.investmentprinciple.dto.mapper.InvestmentPrincipleMapper;
 import com.ogd.stockdiary.application.investmentprinciple.dto.request.CreateMultiplePrinciplesRequest;
@@ -86,7 +77,7 @@ public class InvestmentPrincipleController {
         return HttpApiResponse.of(responses);
     }
 
-    @PutMapping("/{principleId}")
+    @PatchMapping("/{principleId}")
     @Operation(summary = "투자원칙 수정", description = "투자원칙의 내용을 수정합니다.")
     public HttpApiResponse<InvestmentPrincipleResponse> updatePrinciple(
         @PathVariable Long principleId, @Valid @RequestBody UpdatePrincipleRequest request) {
