b257e876db1458ed4f7fca7aca4f642e8bfc1571
nick <andantej281@gmail.com>
2025-11-05 23:27:22 +0900

    fix. 시스템 투자 그룹 조회 응답구조 수정

--- stock-diary/src/main/java/com/ogd/stockdiary/application/principlegroup/dto/response/RecommendedPrincipleGroupResponse.java
+++ stock-diary/src/main/java/com/ogd/stockdiary/application/principlegroup/dto/response/RecommendedPrincipleGroupResponse.java
@@ -1,7 +1,5 @@
 package com.ogd.stockdiary.application.principlegroup.dto.response;
 
-import com.ogd.stockdiary.domain.investmentprinciple.entity.PrincipleType;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,18 +15,9 @@ public class RecommendedPrincipleGroupResponse {
     @Schema(description = "그룹명", example = "워렌 버핏의 가치투자 원칙")
     private final String groupName;
 
-    @Schema(description = "투자원칙 타입 (BUY: 매수, SELL: 매도)", example = "BUY")
-    private final PrincipleType principleType;
-
     @Schema(description = "썸네일 (이모지 또는 이미지 URL)", example = "📈")
     private final String thumbnail;
 
-    @Schema(description = "그룹 표시 순서", example = "1")
-    private final Integer displayOrder;
-
     @Schema(description = "그룹에 속한 투자원칙 개수", example = "5")
     private final Integer principleCount;
-
-    @Schema(description = "투자자/전문가 이름", example = "워렌 버핏")
-    private final String userName;
 }
