b0c57c6069e5a587c33a36bcbe503d279a16e185
yeonsoo <dfh56541@gmail.com>
2025-11-14 09:45:02 +0900

    feat: PrincipleCheck 테이블에 retrospectionId 기준 인덱스 추가

--- stock-diary/src/main/java/com/ogd/stockdiary/domain/principlecheck/entity/PrincipleCheck.java
+++ stock-diary/src/main/java/com/ogd/stockdiary/domain/principlecheck/entity/PrincipleCheck.java
@@ -2,21 +2,7 @@ package com.ogd.stockdiary.domain.principlecheck.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.ConstraintMode;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import com.ogd.stockdiary.domain.investmentprinciple.entity.InvestmentPrinciple;
 import com.ogd.stockdiary.domain.retrospection.entity.Retrospection;
@@ -25,7 +11,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "principle_checks")
+@Table(name = "principle_checks", indexes = {
+    @Index(name = "ix_principle_checks_retrospection_id", columnList = "retrospection_id")
+})
 @Getter
 @NoArgsConstructor
 public class PrincipleCheck {
